8029ea21dab1a1ffdaced30ef423822ddc4de551
Amish L <92310523+amish-go@users.noreply.github.com>
2025-12-05 23:32:30 +0545

    Add files via upload

---
+++ 2_simple_values/main.go
@@ -0,0 +1,14 @@
+package main
+import"fmt"
+func main(){
+	fmt.Println(1+1*(8/2))
+	//simple value
+	fmt.Println("Hello")
+	//string
+	fmt.Println(true)
+	fmt.Println(false)
+	//bool
+	fmt.Println(10.33)
+	fmt.Println(7.3+1.1)
+	//floats
+}
